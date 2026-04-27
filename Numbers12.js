8e0dac21c903479d1d6874e0e04a51387ba7e468
Abhishek Jaiswal <abhishekjaiswal68774@gmail.com>
2026-04-27 21:33:57 +0530

    docs: document Number methods and Math.random() range logic

---
+++ Numbers12.js
@@ -0,0 +1,29 @@
+const balance = new Number(200)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+// console.log(balance.toFixed(5)) // imp in ecom
+
+const otherNumber = 123.89745
+
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++++++++  MATHS ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-9))
+// console.log(Math.floor(4.2))
+// console.log(Math.ceil(4.9))
+// console.log(Math.sqrt(25))
+// console.log(Math.max(7,8,5,2,4,1,111))
+// console.log(Math.min(7,8,5,2,4,1,111))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random()*10) + 1))
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min)

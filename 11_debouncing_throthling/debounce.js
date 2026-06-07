4e78dd29c25b100274f4f38a7d3073701bf0bfe7
Abhishek Jaiswal <abhishekjaiswal68774@gmail.com>
2026-06-07 14:00:48 +0530

    docs: add custom debounce utility for search query optimization

---
+++ 11_debouncing_throthling/debounce.js
@@ -0,0 +1,98 @@
+// Debouncing 
+// "Debouncing is a performance optimization technique used to limit the execution rate of an expensive function. 
+// Structurally, it guarantees that a target function will only execute after a specific window of total inactivity has passed. 
+// If the user creates any interruption or event fire during that countdown, the existing timer is immediately cleared and reset back to zero."
+// So basically decouncing is when a user is not typig for our desired time then only we will call the api or fn,
+// if user intrupted in between then we will reset the timer
+
+function debounce(fn,delay){
+    let timerId;
+
+    return function(...args){
+        clearTimeout(timerId) //cancel the last call
+      timerId = setTimeout(()=>{
+            fn(...args)
+        },delay)
+    }
+}
+
+const search = (query)=>{
+    console.log(`Seraching for ${query}`)
+}
+
+const serachWithDebounce = debounce(search,1000)
+
+serachWithDebounce("H")
+serachWithDebounce("Ha")
+serachWithDebounce("Har")
+serachWithDebounce("Hard")
+serachWithDebounce("Hard ")
+serachWithDebounce("Hard J")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+serachWithDebounce("Hard Js")
+
+
+// function debounce(fn,delay){
+//     let timerId;
+
+//     return function(args){
+//         clearTimeout(timerId); //cancel the last call
+//        timerId = setTimeout(()=>{
+//         fn(...args)
+//        },delay)
+//     }
+// }
+// const serach = (query)=>{
+// console.log(`Searching for ${query}`)
+// }
+
+// const serachWithDebounce = debounce(serach,1000)
+
+// serachWithDebounce("H")
+// serachWithDebounce("Ha")
+// serachWithDebounce("Har")
+// serachWithDebounce("Hard")
+// serachWithDebounce("Hard ")
+// serachWithDebounce("Hard J")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
+// serachWithDebounce("Hard Js")
